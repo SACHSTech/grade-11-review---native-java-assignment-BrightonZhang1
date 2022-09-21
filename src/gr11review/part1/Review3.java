de47086a9d82c120925144218654118ecdf367b9
BrightonZhang1 <75390931+BrightonZhang1@users.noreply.github.com>
2022-09-21 10:34:51 -0400

    Update Review3.java
    fixing indentation

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -3,14 +3,14 @@ import java.io.*;
 
 public class Review3{
 	public static void main(String[] args) throws IOException{
-        for(int i = 21; i < 100; i += 2){
-            System.out.println(i);
-        }
+		for(int i = 21; i < 100; i += 2){
+		    System.out.println(i);
+		}
 
-        System.out.println();
+		System.out.println();
 
-        for(int i = 29; i >= 2; i--){
-            System.out.println(i);
-        }
+		for(int i = 29; i >= 2; i--){
+		    System.out.println(i);
+		}
 	}
 }

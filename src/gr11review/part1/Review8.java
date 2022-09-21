5cb4f2e3544fe060744b231120872f3307ab3695
BrightonZhang1 <75390931+BrightonZhang1@users.noreply.github.com>
2022-09-21 10:41:52 -0400

    Update Review8.java

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -3,16 +3,24 @@ import java.io.*;
 
 public class Review8{
 	public static void main(String[] args) throws IOException{
+		// variable declaration
 		int intCounter = 0;
+
+		// for loop rolling 3 numbers 1000 times
 		for(int i = 0; i < 1000; i++){
+
+			// new random number generated every loop
 			int intNumOne = (int) Math.round(Math.random() * 8);
 			int intNumTwo = (int) Math.round(Math.random() * 8);
 			int intNumThree = (int) Math.round(Math.random() * 8);
 			System.out.println(intNumOne + " " + intNumTwo + " " + intNumThree);
+
+			// if statement checking if numbers match
 			if(intNumOne == intNumTwo && intNumTwo == intNumThree){
 				intCounter++;
 			}
 		}
+		// outputs the number of matching rolls
 		System.out.println(intCounter);
 	}
 }

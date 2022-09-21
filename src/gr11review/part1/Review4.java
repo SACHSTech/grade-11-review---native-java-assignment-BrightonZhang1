704f615012d9c6233bb080ab05db07f313e8e4d0
BrightonZhang1 <75390931+BrightonZhang1@users.noreply.github.com>
2022-09-21 10:35:39 -0400

    Update Review4.java
    fixing indentation

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -4,27 +4,27 @@ import java.text.DecimalFormat;
 
 public class Review4{
 	public static void main(String[] args) throws IOException{
-        // decimal format and buffered reader objects created
-        DecimalFormat df = new DecimalFormat("0.00");
-	    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		// decimal format and buffered reader objects created
+		DecimalFormat df = new DecimalFormat("0.00");
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        // variable declaration
-        int intItemNum;
-        double dblsubTotal = 0;
+		// variable declaration
+		int intItemNum;
+		double dblsubTotal = 0;
 
-        // user input
+		// user input
 		System.out.print("How many items do you want to buy? ");
-        intItemNum = Integer.parseInt(keyboard.readLine());
-        
-        // for loop taking user input per item and adding to subtotal
-        for(int i = 0; i < intItemNum; i++){
-            System.out.print("Enter the price for item " + (i + 1) + ": ");
-            dblsubTotal += Double.parseDouble(keyboard.readLine());
-        }
+		intItemNum = Integer.parseInt(keyboard.readLine());
 
-        // output and calculations with and without tax
-        System.out.println("Subtotal: $" + df.format(dblsubTotal));
-        System.out.println("Tax: $" + df.format(dblsubTotal * 0.13));
-        System.out.println("Total: $" + df.format(dblsubTotal * 1.13));
-    }
+		// for loop taking user input per item and adding to subtotal
+		for(int i = 0; i < intItemNum; i++){
+		    System.out.print("Enter the price for item " + (i + 1) + ": ");
+		    dblsubTotal += Double.parseDouble(keyboard.readLine());
+		}
+
+		// output and calculations with and without tax
+		System.out.println("Subtotal: $" + df.format(dblsubTotal));
+		System.out.println("Tax: $" + df.format(dblsubTotal * 0.13));
+		System.out.println("Total: $" + df.format(dblsubTotal * 1.13));
+    	}
 }

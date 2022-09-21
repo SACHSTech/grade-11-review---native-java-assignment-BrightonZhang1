f58f50f7c6efcf8d71d1650a6478dddd78c18f7f
BrightonZhang1 <75390931+BrightonZhang1@users.noreply.github.com>
2022-09-21 10:40:47 -0400

    Update Review6.java

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -4,24 +4,24 @@ import java.text.DecimalFormat;
 
 public class Review6{
 	public static void main(String[] args) throws IOException{
-        // decimal format and buffered reader object creation
-        DecimalFormat df = new DecimalFormat("0.00");
+		// decimal format and buffered reader object creation
+		DecimalFormat df = new DecimalFormat("0.00");
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        // variable declaration
-        double dblsubTotal = 0;
-        double dblPrice;
-        
-        // do while loop to calculate price until user types 0
-        do{
-            System.out.print("Enter the price for an item: ");
-            dblPrice = Double.parseDouble(keyboard.readLine());
-            dblsubTotal += dblPrice;
-        }while(dblPrice != 0);
+		// variable declaration
+		double dblsubTotal = 0;
+		double dblPrice;
 
-        // output prices with and without tax
-        System.out.println("Subtotal: $" + df.format(dblsubTotal));
-        System.out.println("Tax: $" + df.format(dblsubTotal * 0.13));
-        System.out.println("Total: $" + df.format(dblsubTotal * 1.13));
+		// do while loop to calculate price until user types 0
+		do{
+		    System.out.print("Enter the price for an item: ");
+		    dblPrice = Double.parseDouble(keyboard.readLine());
+		    dblsubTotal += dblPrice;
+		}while(dblPrice != 0);
+
+		// output prices with and without tax
+		System.out.println("Subtotal: $" + df.format(dblsubTotal));
+		System.out.println("Tax: $" + df.format(dblsubTotal * 0.13));
+		System.out.println("Total: $" + df.format(dblsubTotal * 1.13));
 	}
 }

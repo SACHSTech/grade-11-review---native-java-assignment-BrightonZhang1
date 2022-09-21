f2b523efde5024bf8fad8fb741b87e4e05a9345e
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2022-09-21 10:30:27 -0400

    FINAL ASSIGNMENT

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -3,17 +3,21 @@ import java.io.*;
 
 public class Review1{
 	public static void main(String[] args) throws IOException{
+		// buffered reader object creation
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
+		// variable declaration
 		int intMonthNum;
 		int intDayNum;
 
+		// input statements using buffered reader
 		System.out.print("Enter the month number: ");
 		intMonthNum = Integer.parseInt(keyboard.readLine());
 
         System.out.print("Enter the day number: ");
         intDayNum = Integer.parseInt(keyboard.readLine());
 
+		// if statements to calculate specific month days
 		int intTotalDays = intDayNum;
 		if(intMonthNum > 1){
 			for(int i = 0; i < intMonthNum; i++){
@@ -34,6 +38,8 @@ public class Review1{
 				} 
 			}
 		}
+
+		// output total days
 		System.out.println(intTotalDays);
 	}
 }

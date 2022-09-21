f9c488302db6e9fbebec2f75fee1624d1c0a245e
BrightonZhang1 <75390931+BrightonZhang1@users.noreply.github.com>
2022-09-21 10:36:33 -0400

    Update Review5.java

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -5,37 +5,37 @@ public class Review5{
 	public static void main(String[] args) throws IOException{
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        int intYearlyAmount;
-        double dblCompound;
-        int intTarget;
-        double dblMoney = 0;
-        int intYear = 0;
-        
-        System.out.print("Enter the yearly invested amount: ");
-        intYearlyAmount = Integer.parseInt(keyboard.readLine());
+		int intYearlyAmount;
+		double dblCompound;
+		int intTarget;
+		double dblMoney = 0;
+		int intYear = 0;
 
-        System.out.print("Enter the compound interest rate: ");
-        dblCompound = (Double.parseDouble(keyboard.readLine())) / 100;
+		System.out.print("Enter the yearly invested amount: ");
+		intYearlyAmount = Integer.parseInt(keyboard.readLine());
 
-        System.out.print("Enter the target amount: ");
-        intTarget = Integer.parseInt(keyboard.readLine());
+		System.out.print("Enter the compound interest rate: ");
+		dblCompound = (Double.parseDouble(keyboard.readLine())) / 100;
 
-        if(intTarget != 0){
-            while(dblMoney <= intTarget){
-                dblMoney += intYearlyAmount;
+		System.out.print("Enter the target amount: ");
+		intTarget = Integer.parseInt(keyboard.readLine());
 
-                if(dblCompound != 0){
-                    dblMoney *= (dblCompound + 1);
-                }
-                
-                intYear++;
-            }
-        }
+		if(intTarget != 0){
+		    while(dblMoney <= intTarget){
+			dblMoney += intYearlyAmount;
 
-        if(dblCompound == 0 && dblMoney != 0 && intTarget != 0){
-            intYear--;
-        }
+			if(dblCompound != 0){
+			    dblMoney *= (dblCompound + 1);
+			}
 
-        System.out.println("The target amount will be earned in " + intYear + " years.");
+			intYear++;
+		    }
+		}
+
+		if(dblCompound == 0 && dblMoney != 0 && intTarget != 0){
+		    intYear--;
+		}
+
+		System.out.println("The target amount will be earned in " + intYear + " years.");
 	}
 }
